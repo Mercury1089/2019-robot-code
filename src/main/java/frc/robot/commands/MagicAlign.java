f88ad62315daa1d47118ea04f54856b1dcee0b43
Mercury 1089 <1089software@gmail.com>
2019-01-30 16:55:29 -0500

    Magic align wip

--- src/main/java/frc/robot/commands/MagicAlign.java
+++ src/main/java/frc/robot/commands/MagicAlign.java
@@ -41,9 +41,21 @@ public class MagicAlign extends Command {
     left.configPeakOutputForward(+1.0, 10);
 		left.configPeakOutputReverse(-1.0, 10);
 		right.configPeakOutputForward(+1.0, 10);
-		right.configPeakOutputReverse(-1.0, 10);
+    right.configPeakOutputReverse(-1.0, 10);
     
+    left.getSensorCollection().setQuadraturePosition(0, 10);
+    right.getSensorCollection().setQuadraturePosition(0, 10);
     
+    right.selectProfileSlot(Constants.kSlot_Distanc, Constants.PID_PRIMARY);
+		right.selectProfileSlot(Constants.kSlot_Turning, Constants.PID_TURN);
+			
+		/* Calculate targets from gamepad inputs */
+		double target_sensorUnits = forward * Constants.kSensorUnitsPerRotation * Constants.kRotationsToTravel;
+		double target_turn = _targetAngle;
+		
+		/* Configured for MotionMagic on Quad Encoders' Sum and Auxiliary PID on Pigeon */
+		_rightMaster.set(ControlMode.MotionMagic, target_sensorUnits, DemandType.AuxPID, target_turn);
+		_leftMaster.follow(_rightMaster, FollowerType.AuxOutput1);
   }
 
   // Called repeatedly when this Command is scheduled to run
