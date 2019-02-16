adc39ec6ae1b4f38a0c9f4270abf44473abef425
Team Mercury 1089 <1089software@gmail.com>
2019-02-16 09:33:27 -0500

    Work on auton command

--- src/main/java/frc/robot/auton/AutonCommand.java
+++ src/main/java/frc/robot/auton/AutonCommand.java
@@ -8,14 +8,27 @@ import java.io.FileNotFoundException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.graalvm.compiler.nodes.calc.LeftShiftNode;
 
 /**
  * Command group that specifies the commands to be run
  * during the autonomous period.
  */
 public class AutonCommand extends CommandGroup {
+    
     private static Logger log = LogManager.getLogger(AutonCommand.class);
-
+    private int rotationFactor;
+    private final double CARGO_PICKUP_X_OFFSET = 0,
+        //Change these values, just placeholders
+        CARGO_PICKUP_X_CONSTANT_OFFSET = 0,
+        CARGO_PICKUP_Y_CHANGING_OFFSET = 0,
+        CARGO_PICKUP_Y_CONSTANT_OFFSET = 0,
+        LEFT_CARGO_SHIP_OFFSET = 0,
+        RIGHT_CARGO_SHIP_OFFSET = 0,
+        LEFT_ROCKET_OFFSET = 0,
+        RIGHT_ROCKET_OFFSET = 0,
+        OFF_HAB = 0;
+        
     public enum AutonStartSide {
         Left, Mid, Right
     }
@@ -25,6 +38,22 @@ public class AutonCommand extends CommandGroup {
     }
 
     public AutonCommand(AutonStartSide autonSide, MoveLocation[] moveLocations) {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
         // FIRST MOVE
         try {
             addSequential(new MoveOnPath(autonSide.name() + moveLocations[0].name(), MPDirection.FORWARD));
@@ -33,5 +62,6 @@ public class AutonCommand extends CommandGroup {
             return;
             // In the future we COULD do a stupid check to make sure user didn't say " Right Middle " or so
         }
+        //make a switch case with cases for what side the robot started on ex. middle
     }
 }
