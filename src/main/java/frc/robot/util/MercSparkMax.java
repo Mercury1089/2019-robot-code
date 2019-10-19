71c34da2bef9132d8b6dfe2596cfe94d9987ef49
Ishaq <ishaqmeeran03@gmail.com>
2019-10-18 20:23:49 -0400

    Added support for Sparks

---
+++ src/main/java/frc/robot/util/MercSparkMax.java
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.util;
+
+import com.ctre.phoenix.ParamEnum;
+import com.ctre.phoenix.motorcontrol.*;
+import com.revrobotics.CANSparkMax;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.subsystems.DriveTrain.DriveTrainLayout;
+import frc.robot.util.interfaces.IMercMotorController;
+
+/**
+ * Add your docs here.
+ */
+public class MercSparkMax implements IMercMotorController {
+    public MercSparkMax() {
+        
+    }
+}
